965317f25c346a46fa89c59bd577c915f787f5ea
themhpro <cse.mdmehedi@gmail.com>
2019-11-05 21:02:12 +0600

    Add

---
+++ ImportExcelDataToGridView/Home.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace ImportExcelDataToGridView
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.exitButton = new Guna.UI.WinForms.GunaButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.showDataGridView = new System.Windows.Forms.DataGridView();
+            this.SL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Contact = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.showDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(159)))), ((int)(((byte)(91)))));
+            this.panel1.Controls.Add(this.exitButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(950, 25);
+            this.panel1.TabIndex = 0;
+            // 
+            // exitButton
+            // 
+            this.exitButton.AnimationHoverSpeed = 0.07F;
+            this.exitButton.AnimationSpeed = 0.03F;
+            this.exitButton.BaseColor = System.Drawing.Color.Transparent;
+            this.exitButton.BorderColor = System.Drawing.Color.Black;
+            this.exitButton.FocusedColor = System.Drawing.Color.Empty;
+            this.exitButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.exitButton.ForeColor = System.Drawing.Color.White;
+            this.exitButton.Image = ((System.Drawing.Image)(resources.GetObject("exitButton.Image")));
+            this.exitButton.ImageAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.exitButton.ImageSize = new System.Drawing.Size(20, 20);
+            this.exitButton.Location = new System.Drawing.Point(926, 0);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.OnHoverBaseColor = System.Drawing.Color.Red;
+            this.exitButton.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.exitButton.OnHoverForeColor = System.Drawing.Color.White;
+            this.exitButton.OnHoverImage = null;
+            this.exitButton.OnPressedColor = System.Drawing.Color.Black;
+            this.exitButton.Size = new System.Drawing.Size(25, 25);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.linkLabel1);
+            this.panel2.Controls.Add(this.showDataGridView);
+            this.panel2.Location = new System.Drawing.Point(1, 24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(948, 525);
+            this.panel2.TabIndex = 1;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(41, 15);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(133, 17);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Import Data from File";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // showDataGridView
+            // 
+            this.showDataGridView.AllowUserToAddRows = false;
+            this.showDataGridView.AllowUserToDeleteRows = false;
+            this.showDataGridView.BackgroundColor = System.Drawing.Color.White;
+            this.showDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.showDataGridView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(159)))), ((int)(((byte)(91)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(159)))), ((int)(((byte)(91)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.showDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.showDataGridView.ColumnHeadersHeight = 30;
+            this.showDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.showDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.SL,
+            this.Name,
+            this.Contact,
+            this.Email,
+            this.Address});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Azure;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.SeaShell;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.showDataGridView.DefaultCellStyle = dataGridViewCellStyle2;
+            this.showDataGridView.EnableHeadersVisualStyles = false;
+            this.showDataGridView.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(159)))), ((int)(((byte)(91)))));
+            this.showDataGridView.Location = new System.Drawing.Point(11, 39);
+            this.showDataGridView.MultiSelect = false;
+            this.showDataGridView.Name = "showDataGridView";
+            this.showDataGridView.ReadOnly = true;
+            this.showDataGridView.RowHeadersVisible = false;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.MintCream;
+            this.showDataGridView.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.showDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.showDataGridView.Size = new System.Drawing.Size(926, 486);
+            this.showDataGridView.TabIndex = 2;
+            // 
+            // SL
+            // 
+            this.SL.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.SL.HeaderText = "#";
+            this.SL.Name = "SL";
+            this.SL.ReadOnly = true;
+            this.SL.Width = 39;
+            // 
+            // Name
+            // 
+            this.Name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Name.HeaderText = "Name";
+            this.Name.Name = "Name";
+            this.Name.ReadOnly = true;
+            // 
+            // Contact
+            // 
+            this.Contact.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Contact.HeaderText = "Contact";
+            this.Contact.Name = "Contact";
+            this.Contact.ReadOnly = true;
+            // 
+            // Email
+            // 
+            this.Email.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Email.HeaderText = "E-Mail";
+            this.Email.Name = "Email";
+            this.Email.ReadOnly = true;
+            // 
+            // Address
+            // 
+            this.Address.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Address.HeaderText = "Address";
+            this.Address.Name = "Address";
+            this.Address.ReadOnly = true;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(159)))), ((int)(((byte)(91)))));
+            this.ClientSize = new System.Drawing.Size(950, 550);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.showDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI.WinForms.GunaButton exitButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView showDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Contact;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ ImportExcelDataToGridView/Home.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace ImportExcelDataToGridView
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Do You Want to Exit ?", "Exit Application",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if(dialogResult == DialogResult.Yes)
+                this.Dispose();
+        }
+
+        OpenFileDialog _openFileDialog = new OpenFileDialog();
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            showDataGridView.Rows.Clear();
+
+            Microsoft.Office.Interop.Excel.Application xlApplication;
+            Microsoft.Office.Interop.Excel.Workbook xlWorkbook;
+            Microsoft.Office.Interop.Excel.Worksheet xlWorksheet;
+            Microsoft.Office.Interop.Excel.Range xlRange;
+
+            int xlRow;
+            string fileName;
+
+            _openFileDialog.Filter = "Excel Office | *.xl; *.xlsx";
+            _openFileDialog.ShowDialog();
+            fileName = _openFileDialog.FileName;
+
+            if (fileName != string.Empty)
+            {
+                xlApplication = new Microsoft.Office.Interop.Excel.Application();
+                xlWorkbook = xlApplication.Workbooks.Open(fileName);
+                xlWorksheet = xlWorkbook.Worksheets["Sheet1"];
+                xlRange = xlWorksheet.UsedRange;
+
+                int i = 0;
+
+                for (xlRow = 2; xlRow <= xlRange.Rows.Count; xlRow++)
+                {
+                    if (xlRange.Cells[xlRow, 1].Text != "")
+                    {
+                        i++;
+                        showDataGridView.Rows.Add(i, xlRange.Cells[xlRow, 1].Text, xlRange.Cells[xlRow, 2].Text,
+                            xlRange.Cells[xlRow, 3].Text, xlRange.Cells[xlRow, 4].Text);
+                    }
+                }
+                xlWorkbook.Close();
+                xlApplication.Quit();
+            }
+        }
+    }
+}
